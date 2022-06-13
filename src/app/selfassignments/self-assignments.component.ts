650ef5172b07aac4f51c18ff9f17dcec725b2dcc
Changwoo Lee <95376661+MyMoose@users.noreply.github.com>
2022-06-12 21:27:21 -0700

    angular

---
+++ src/app/selfassignments/self-assignments.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'self-assignment',
+    templateUrl: './self-assignments.component.html',
+    
+})
+
+export class SelfAssignments { }
